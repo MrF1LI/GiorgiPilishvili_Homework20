8edf9ffdaafecd38d2e47f64bc90b4837339bb90
Giorgi Pilishvili <giorgi.pilishvili.1@btu.edu.ge>
2022-08-09 13:39:00 +0400

    Homework 20

--- GiorgiPilishvili_Homework20/ViewController.swift
+++ GiorgiPilishvili_Homework20/ViewController.swift
@@ -8,12 +8,57 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+        
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+        
+    func printPrimeNumbers() {
+        for num in 1...100_000 {
+            if num.isPrime() {
+                print(num)
+            }
+        }
+    }
+    
+    // MARK: Actions
 
-
+    @IBAction func actionButton(_ sender: UIButton) {
+        
+        sender.isEnabled = false
+        
+        DispatchQueue.global().async {
+            self.printPrimeNumbers()
+            DispatchQueue.main.async {
+                sender.isEnabled = true
+            }
+        }
+        
+    }
+    
 }
 
+// MARK: - Extension for check prime number
+
+extension Int {
+    
+    func isPrime() -> Bool {
+        
+        if self == 1 {
+            return false
+        }
+        
+        for num in 2..<self {
+            if self % num == 0 {
+                return false
+            }
+        }
+        
+        return true
+        
+    }
+    
+}
